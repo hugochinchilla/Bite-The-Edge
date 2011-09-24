e4ed4c15c3a38238cab7f484861e201179cf1767
Hugo Chinchilla Carbonell <hugo@bulma.net>
2011-09-24 14:31:44 +0200

    version 0.2

--- footer.php
+++ footer.php
@@ -15,10 +15,13 @@
 	<footer id="colophon" role="contentinfo">
 		<div id="site-generator">
 			<?php do_action( 'toolbox_credits' ); ?>
-			<?php printf( __( 'Theme %1$s by %2$s.', 'toolbox' ), 'Bite The Edge', '<a href="http://www.hugochinchilla.net/" rel="designer">Hugo Chinchilla</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'toolbox' ), 'Bite The Edge', '<a href="http://www.hugochinchilla.net/" rel="designer">Hugo Chinchilla</a>' ); ?>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
+
+<script type="text/javascript" src="http://use.typekit.com/tmh5xcy.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
 <?php wp_footer(); ?>
 
